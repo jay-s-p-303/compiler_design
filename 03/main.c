0ea0070e25cd6ff566f0116445a02f6d8197707d
Jay Parmar <99414670+jay-s-p-303@users.noreply.github.com>
2023-06-22 05:05:34 +0000

    Fix prog. 3

--- 03/main.c
+++ 03/main.c
@@ -3,16 +3,11 @@
 void main()
 {
     char str[20];
-    int i = 0;
     printf("Enter the string: ");
     scanf("%s", str);
     // if((str[0]>='a' && str[0]<='z') || (str[0]>='A' && str[0]<='Z') || str[0]=='_')
     if ((str[0] >= 65 && str[0] <= 90) || (str[0] >= 97 && str[0] <= 122) || str[0] == 95)
-    {
         printf("Valid identifier\n");
-    }
     else
-    {
         printf("Invalid identifier\n");
-    }
 }
